140018d7ef6e179d0d3415f16fc286212060eb88
Bram-Vanden-Broeck <152525728+Bram-Vanden-Broeck@users.noreply.github.com>
2025-04-10 21:18:01 +0200

    Update TelescopeServiceProvider.php

--- laravel-backpack-telescope-production/TelescopeServiceProvider.php
+++ laravel-backpack-telescope-production/TelescopeServiceProvider.php
@@ -55,7 +55,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                'bram@celegens.com',
+                'developer@celegens.com',
             ]);
         });
     }
